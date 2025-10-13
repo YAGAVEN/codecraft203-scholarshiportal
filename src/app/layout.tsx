780bbce559c581e25415f9600a8f51ad482b1557
Pritto Ruban <prittoprogrammer@gmail.com>
2025-10-13 19:40:21 +0530

    feat: Add Voiceflow chatbot integration to the RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
@@ -36,6 +37,32 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        
+        {/* Voiceflow Chatbot */}
+        <Script
+          id="voiceflow-chatbot"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(d, t) {
+                var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+                v.onload = function() {
+                  window.voiceflow.chat.load({
+                    verify: { projectID: '68ece578ff97cf74d0874b25' },
+                    url: 'https://general-runtime.voiceflow.com',
+                    versionID: 'production',
+                    voice: {
+                      url: "https://runtime-api.voiceflow.com"
+                    }
+                  });
+                }
+                v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs";
+                v.type = "text/javascript";
+                s.parentNode.insertBefore(v, s);
+              })(document, 'script');
+            `,
+          }}
+        />
       </body>
     </html>
   );
